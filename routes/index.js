3acf5d2e75d3e9e58d48f14cf16e9c318e1e1308
Shizurux <Shizurux02@gmail.com>
2016-03-17 23:16:18 +1300

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+var basex = require('basex');
+var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+client.execute("OPEN Colenso");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'The Colenso TEI Database' });
+});
+
+module.exports = router;
